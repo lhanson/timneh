d0976e12f9ae93ef8d9550f9bee07e43ea8b0731
Lyle Hanson <lyle.hanson@wisc.edu>
2016-10-18 10:42:38 -0500

    Added new columns to data DDL

--- src/main/resources/data-postgresql-from-scratch.sql
+++ src/main/resources/data-postgresql-from-scratch.sql
@@ -1,6 +1,6 @@
 /*
  * Populates a fresh version of the Timneh database for testing
  */
-insert into users values(1, 'user', 'password', true, current_timestamp);
+insert into users values(1, 'user', 'password', 'full name', 'email@address.tld', true, current_timestamp);
 insert into authorities values(1, 'USER');
 insert into discussions values (1, 1, 'Test Discussion', current_timestamp, null)
