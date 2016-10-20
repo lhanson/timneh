2f8973d6d14c95de831cd7d7d5246a5ff82a6c17
Lyle Hanson <lyle.hanson@wisc.edu>
2016-10-19 22:34:30 -0500

    Remapping postgres port when running on Travis CI

--- docker/postgresql/build_run.sh
+++ docker/postgresql/build_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Builds and runs our custom PostgreSQL Docker container
+# Builds and runs our custom PostgreSQL Docker container.
+# Takes a single argument, the port number for Postgres to listen on.
 
 set -e
 docker build --tag timneh-postgresql docker/postgresql
-docker run --detach --publish 6666:5432 timneh-postgresql
+docker run --detach --publish $1:5432 timneh-postgresql
