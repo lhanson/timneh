4258eadf1170c0368b0504b141486e3521b27af7
Lyle Hanson <lyle.hanson@wisc.edu>
2016-10-19 22:03:10 -0500

    Trying a different postgres posrt on docker

--- docker/postgresql/build_run.sh
+++ docker/postgresql/build_run.sh
@@ -3,4 +3,4 @@
 
 set -e
 docker build --tag timneh-postgresql docker/postgresql
-docker run --publish 127.0.0.1:5432:5432 --rm timneh-postgresql
+docker run --publish 9876:5432 --rm timneh-postgresql
