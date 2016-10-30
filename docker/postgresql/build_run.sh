62499aa9950cdb88e4e1d27d01bd59eb0bedf551
Lyle Hanson <lyle.hanson@wisc.edu>
2016-10-30 07:22:01 -0500

    Build script is $CWD agnostic. Postgres 9.6 apparently includes CITEXT.

--- docker/postgresql/build_run.sh
+++ docker/postgresql/build_run.sh
@@ -3,6 +3,7 @@
 # Takes a single argument, the port number for Postgres to listen on.
 
 set -e
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use port 5432 unless specified
 postgres_port="5432"
@@ -10,5 +11,5 @@ if [ -n "$1" ]; then
 	postgres_port="$1"
 fi
 
-docker build --tag timneh-postgresql docker/postgresql
-docker run --detach --publish ${postgres_port}:5432 timneh-postgresql
+docker build --tag timneh-postgresql $SCRIPTDIR
+docker run --detach --publish ${postgres_port}:5432 --name timneh-postgresql timneh-postgresql
